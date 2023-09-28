728ea8977729b82d2e6e1de5bef93ec8a0033af0
CosmodLima <cosmodlima016@gmail.com>
2023-09-28 20:47:13 -0300

    Resolvendo desafio listaTarefa
    Signed-off-by: CosmodLima <cosmodlima016@gmail.com>

---
+++ src/main/java/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,18 @@
+package main.java.list.OperacoesBasicas;
+
+public class Tarefa {
+
+  private String descrição;
+
+  public Tarefa(String descrição){
+    this.descrição = descrição;
+  }
+
+  public String getDescricao()  {
+    return descrição;
+  }  
+
+  public String toString() {
+    return descrição;
+  }
+}
